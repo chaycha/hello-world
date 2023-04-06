9d6d833296a4c002c82b8adf05d0419dc6f0f479
chaycha <chayapol.com@gmail.com>
2023-04-06 14:15:18 +0800

    third try

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -56,9 +56,6 @@ export default function ToDoList() {
           }
         </ul>
       </TaskFrame>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
